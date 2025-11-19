6b0d63eee6eb0f10079567bea57b997a98c16380
ItsAddii <fowler.addison13@gmail.com>
2025-11-18 19:47:50 -0600

    hero section started

---
+++ my-portfolio/src/components/hero.jsx
@@ -0,0 +1,12 @@
+import "./hero.css";
+
+export default function Hero() {
+  return (
+    <section className="heroSection">
+        <div className="heroText">
+            <h1>Hey, I'm Addi!</h1>
+            <h2>I'm a self-taught Front-End Developer.</h2>
+        </div>
+    </section>
+  );
+}
